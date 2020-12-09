55d1968431ab9a3512e447f9ac2a3183ea037963
Quentin Gruber <quentingruber@gmail.com>
2020-12-09 19:09:49 +0100

    init

---
+++ H1Emu/MainForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace H1Emu
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.startDownloadBtn = new System.Windows.Forms.Button();
+            this.usernameText = new System.Windows.Forms.TextBox();
+            this.passwordText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.argsText = new System.Windows.Forms.TextBox();
+            this.consoleLogs = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startDownloadBtn
+            // 
+            this.startDownloadBtn.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.startDownloadBtn.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.startDownloadBtn.Location = new System.Drawing.Point(259, 251);
+            this.startDownloadBtn.Name = "startDownloadBtn";
+            this.startDownloadBtn.Size = new System.Drawing.Size(284, 23);
+            this.startDownloadBtn.TabIndex = 0;
+            this.startDownloadBtn.Text = "Start Download";
+            this.startDownloadBtn.UseVisualStyleBackColor = false;
+            this.startDownloadBtn.Click += new System.EventHandler(this.startDownloadBtn_Click);
+            // 
+            // usernameText
+            // 
+            this.usernameText.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.usernameText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.usernameText.Location = new System.Drawing.Point(11, 40);
+            this.usernameText.Name = "usernameText";
+            this.usernameText.Size = new System.Drawing.Size(231, 20);
+            this.usernameText.TabIndex = 1;
+            // 
+            // passwordText
+            // 
+            this.passwordText.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.passwordText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordText.Location = new System.Drawing.Point(11, 88);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.PasswordChar = '*';
+            this.passwordText.Size = new System.Drawing.Size(231, 20);
+            this.passwordText.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Steam Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Steam Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Arguments";
+            // 
+            // argsText
+            // 
+            this.argsText.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.argsText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.argsText.Location = new System.Drawing.Point(289, 40);
+            this.argsText.Multiline = true;
+            this.argsText.Name = "argsText";
+            this.argsText.Size = new System.Drawing.Size(423, 68);
+            this.argsText.TabIndex = 7;
+            this.argsText.Text = "-app 295110 -depot 295111 -manifest 1930886153446950288";
+            // 
+            // consoleLogs
+            // 
+            this.consoleLogs.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.consoleLogs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.consoleLogs.Location = new System.Drawing.Point(11, 147);
+            this.consoleLogs.Multiline = true;
+            this.consoleLogs.Name = "consoleLogs";
+            this.consoleLogs.Size = new System.Drawing.Size(428, 87);
+            this.consoleLogs.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Console";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(800, 286);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.consoleLogs);
+            this.Controls.Add(this.argsText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordText);
+            this.Controls.Add(this.usernameText);
+            this.Controls.Add(this.startDownloadBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "H1Emu - Build Depot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startDownloadBtn;
+        private System.Windows.Forms.TextBox usernameText;
+        private System.Windows.Forms.TextBox passwordText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox argsText;
+        private System.Windows.Forms.TextBox consoleLogs;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ H1Emu/MainForm.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace H1Emu
+{
+    public partial class MainForm : Form
+    {
+
+        TextWriter _writer = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _writer = new TextBoxStreamWriter(consoleLogs);
+            Console.SetOut(_writer);
+        }
+
+        private void startDownloadBtn_Click(object sender, EventArgs e)
+        {
+            startDownloadBtn_ClickAsync();
+        }
+
+        private async Task startDownloadBtn_ClickAsync()
+        {
+
+            string [] args = argsText.Text.Split(' ');
+
+            try { 
+            AccountSettingsStore.LoadFromFile("account.config");
+            } catch (Exception er)
+            {
+                Console.WriteLine(er.Message);
+            }
+
+            string username = usernameText.Text;
+            string password = passwordText.Text;
+
+            ContentDownloader.Config.RememberPassword = HasParameter(args, "-remember-password");
+
+            ContentDownloader.Config.DownloadManifestOnly = HasParameter(args, "-manifest-only");
+
+            int cellId = GetParameter<int>(args, "-cellid", -1);
+            if (cellId == -1)
+            {
+                cellId = 0;
+            }
+
+            ContentDownloader.Config.CellID = cellId;
+
+            string fileList = GetParameter<string>(args, "-filelist");
+            string[] files = null;
+
+            if (fileList != null)
+            {
+                try
+                {
+                    string fileListData = File.ReadAllText(fileList);
+                    files = fileListData.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    ContentDownloader.Config.UsingFileList = true;
+                    ContentDownloader.Config.FilesToDownload = new List<string>();
+                    ContentDownloader.Config.FilesToDownloadRegex = new List<Regex>();
+
+                    var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+                    foreach (var fileEntry in files)
+                    {
+                        try
+                        {
+                            string fileEntryProcessed;
+                            if (isWindows)
+                            {
+                                // On Windows, ensure that forward slashes can match either forward or backslashes in depot paths
+                                fileEntryProcessed = fileEntry.Replace("/", "[\\\\|/]");
+                            }
+                            else
+                            {
+                                // On other systems, treat / normally
+                                fileEntryProcessed = fileEntry;
+                            }
+                            Regex rgx = new Regex(fileEntryProcessed, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                            ContentDownloader.Config.FilesToDownloadRegex.Add(rgx);
+                        }
+                        catch
+                        {
+                            // For anything that can't be processed as a Regex, allow both forward and backward slashes to match
+                            // on Windows
+                            if (isWindows)
+                            {
+                                ContentDownloader.Config.FilesToDownload.Add(fileEntry.Replace("/", "\\"));
+                            }
+                            ContentDownloader.Config.FilesToDownload.Add(fileEntry);
+                            continue;
+                        }
+                    }
+
+                    Console.WriteLine("Using filelist: '{0}'.", fileList);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Warning: Unable to load filelist: {0}", ex.ToString());
+                }
+            }
+
+            ContentDownloader.Config.InstallDirectory = GetParameter<string>(args, "-dir");
+
+            ContentDownloader.Config.VerifyAll = HasParameter(args, "-verify-all") || HasParameter(args, "-verify_all") || HasParameter(args, "-validate");
+            ContentDownloader.Config.MaxServers = GetParameter<int>(args, "-max-servers", 20);
+            ContentDownloader.Config.MaxDownloads = GetParameter<int>(args, "-max-downloads", 8);
+            ContentDownloader.Config.MaxServers = Math.Max(ContentDownloader.Config.MaxServers, ContentDownloader.Config.MaxDownloads);
+            ContentDownloader.Config.LoginID = HasParameter(args, "-loginid") ? (uint?)GetParameter<uint>(args, "-loginid") : null;
+
+
+            uint appId = GetParameter<uint>(args, "-app", ContentDownloader.INVALID_APP_ID);
+            if (appId == ContentDownloader.INVALID_APP_ID)
+            {
+                Console.WriteLine("Error: -app not specified!");
+            }
+
+            ulong pubFile = GetParameter<ulong>(args, "-pubfile", ContentDownloader.INVALID_MANIFEST_ID);
+            ulong ugcId = GetParameter<ulong>(args, "-ugc", ContentDownloader.INVALID_MANIFEST_ID);
+            if (pubFile != ContentDownloader.INVALID_MANIFEST_ID)
+            {
+                #region Pubfile Downloading
+
+                if (InitializeSteam(username, password))
+                {
+                    try
+                    {
+                        await ContentDownloader.DownloadPubfileAsync(appId, pubFile).ConfigureAwait(false);
+                    }
+                    catch (Exception ex) when (
+                        ex is ContentDownloaderException
+                        || ex is OperationCanceledException)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    catch (Exception er)
+                    {
+                        Console.WriteLine("Download failed to due to an unhandled exception: {0}", er.Message);
+                        throw;
+                    }
+                    finally
+                    {
+                        ContentDownloader.ShutdownSteam3();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error: InitializeSteam failed");
+                }
+
+                #endregion
+            }
+            else if (ugcId != ContentDownloader.INVALID_MANIFEST_ID)
+            {
+                #region UGC Downloading
+
+                if (InitializeSteam(username, password))
+                {
+                    try
+                    {
+                        await ContentDownloader.DownloadUGCAsync(appId, ugcId).ConfigureAwait(false);
+                    }
+                    catch (Exception ex) when (
+                        ex is ContentDownloaderException
+                        || ex is OperationCanceledException)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    catch (Exception er)
+                    {
+                        Console.WriteLine("Download failed to due to an unhandled exception: {0}", er.Message);
+                        throw;
+                    }
+                    finally
+                    {
+                        ContentDownloader.ShutdownSteam3();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error: InitializeSteam failed");
+                }
+
+                #endregion
+            }
+            else
+            {
+                #region App downloading
+
+                string branch = GetParameter<string>(args, "-branch") ?? GetParameter<string>(args, "-beta") ?? ContentDownloader.DEFAULT_BRANCH;
+                ContentDownloader.Config.BetaPassword = GetParameter<string>(args, "-betapassword");
+
+                ContentDownloader.Config.DownloadAllPlatforms = HasParameter(args, "-all-platforms");
+                string os = GetParameter<string>(args, "-os", null);
+
+                if (ContentDownloader.Config.DownloadAllPlatforms && !String.IsNullOrEmpty(os))
+                {
+                    Console.WriteLine("Error: Cannot specify -os when -all-platforms is specified.");
+                }
+
+                string arch = GetParameter<string>(args, "-osarch", null);
+
+                ContentDownloader.Config.DownloadAllLanguages = HasParameter(args, "-all-languages");
+                string language = GetParameter<string>(args, "-language", null);
+
+                if (ContentDownloader.Config.DownloadAllLanguages && !String.IsNullOrEmpty(language))
+                {
+                    Console.WriteLine("Error: Cannot specify -language when -all-languages is specified.");
+                }
+
+                bool lv = HasParameter(args, "-lowviolence");
+
+                List<(uint, ulong)> depotManifestIds = new List<(uint, ulong)>();
+                bool isUGC = false;
+
+                List<uint> depotIdList = GetParameterList<uint>(args, "-depot");
+                List<ulong> manifestIdList = GetParameterList<ulong>(args, "-manifest");
+                if (manifestIdList.Count > 0)
+                {
+                    if (depotIdList.Count != manifestIdList.Count)
+                    {
+                        Console.WriteLine("Error: -manifest requires one id for every -depot specified");
+                    }
+
+                    var zippedDepotManifest = depotIdList.Zip(manifestIdList, (depotId, manifestId) => (depotId, manifestId));
+                    depotManifestIds.AddRange(zippedDepotManifest);
+                }
+                else
+                {
+                    depotManifestIds.AddRange(depotIdList.Select(depotId => (depotId, ContentDownloader.INVALID_MANIFEST_ID)));
+                }
+
+                if (InitializeSteam(username, password))
+                {
+                    try
+                    {
+                        await ContentDownloader.DownloadAppAsync(appId, depotManifestIds, branch, os, arch, language, lv, isUGC).ConfigureAwait(false);
+                    }
+                    catch (Exception ex) when (
+                        ex is ContentDownloaderException
+                        || ex is OperationCanceledException)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    catch (Exception er)
+                    {
+                        Console.WriteLine("Download failed to due to an unhandled exception: {0}", er.Message);
+                        throw;
+                    }
+                    finally
+                    {
+                        ContentDownloader.ShutdownSteam3();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error: InitializeSteam failed");
+                }
+
+            }
+
+        }
+
+
+        static bool InitializeSteam(string username, string password)
+        {
+            if (username != null && password == null && (!ContentDownloader.Config.RememberPassword || !AccountSettingsStore.Instance.LoginKeys.ContainsKey(username)))
+            {
+                do
+                {
+                    Console.Write("Enter account password for \"{0}\": ", username);
+                    if (Console.IsInputRedirected)
+                    {
+                        password = Console.ReadLine();
+                    }
+                    else
+                    {
+                        // Avoid console echoing of password
+                        password = Util.ReadPassword();
+                    }
+                    Console.WriteLine();
+                } while (String.Empty == password);
+            }
+            else if (username == null)
+            {
+                Console.WriteLine("No username given. Using anonymous account with dedicated server subscription.");
+            }
+
+            // capture the supplied password in case we need to re-use it after checking the login key
+            ContentDownloader.Config.SuppliedPassword = password;
+
+            return ContentDownloader.InitializeSteam3(username, password);
+        }
+
+        static int IndexOfParam(string[] args, string param)
+        {
+            for (int x = 0; x < args.Length; ++x)
+            {
+                if (args[x].Equals(param, StringComparison.OrdinalIgnoreCase))
+                    return x;
+            }
+            return -1;
+        }
+        static bool HasParameter(string[] args, string param)
+        {
+            return IndexOfParam(args, param) > -1;
+        }
+
+        static T GetParameter<T>(string[] args, string param, T defaultValue = default(T))
+        {
+            int index = IndexOfParam(args, param);
+
+            if (index == -1 || index == (args.Length - 1))
+                return defaultValue;
+
+            string strParam = args[index + 1];
+
+            var converter = TypeDescriptor.GetConverter(typeof(T));
+            if (converter != null)
+            {
+                return (T)converter.ConvertFromString(strParam);
+            }
+
+            return default(T);
+        }
+
+        static List<T> GetParameterList<T>(string[] args, string param)
+        {
+            List<T> list = new List<T>();
+            int index = IndexOfParam(args, param);
+
+            if (index == -1 || index == (args.Length - 1))
+                return list;
+
+            index++;
+
+            while (index < args.Length)
+            {
+                string strParam = args[index];
+
+                if (strParam[0] == '-') break;
+
+                var converter = TypeDescriptor.GetConverter(typeof(T));
+                if (converter != null)
+                {
+                    list.Add((T)converter.ConvertFromString(strParam));
+                }
+
+                index++;
+            }
+
+            return list;
+        }
+
+    }
+
+}
+#endregion
