33c198b2ac19e32eb82db11098a0798c48eea9c2
Quentin Gruber <quentingruber@gmail.com>
2020-12-09 21:00:43 +0100

    rename 2/2

--- H1DepotDownloader/Properties/Resources.Designer.cs
+++ H1DepotDownloader/Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace H1DepotDownloader.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("H1Emu.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("H1DepotDownloader.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
